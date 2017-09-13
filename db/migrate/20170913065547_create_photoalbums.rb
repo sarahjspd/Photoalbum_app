dd5a9a88019851acd66b7e97d3624c121d97fad4
Regent Chien <regent.chien@hotmail.sg>
2017-09-13 15:34:02 +0800

    photoalbumpage

---
+++ db/migrate/20170913065547_create_photoalbums.rb
@@ -0,0 +1,14 @@
+class CreatePhotoalbums < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photoalbums do |t|
+
+    	t.string :string
+    	t.text :description
+
+
+    	t.references :user, foreign_key: true
+
+    end
+
+  end
+end
